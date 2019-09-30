e6c84ea5bb577f4066f7ef15ada9c9d771c49f4b
Stephen <sfcline1608@eagle.fgcu.edu>
2019-09-30 14:19:14 -0400

    controller class added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,22 @@
 package com.company;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("GUI.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("test");
+        launch(args);
     }
 }
