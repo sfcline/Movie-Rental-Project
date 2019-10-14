c03033d65338a7d5c11ef2d1080795cd63cc7762
liana <lianamadden@ymail.com>
2019-10-14 18:41:57 -0400

    Button text changes color on mouse enter and exit

--- src/com/company/MovieRentalHomePageController.java
+++ src/com/company/MovieRentalHomePageController.java
@@ -3,8 +3,16 @@ package com.company;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.paint.Color;
+import javax.swing.*;
+import java.awt.*;
 
 public class MovieRentalHomePageController {
+
+    final String orange = "#fc9e4f";
+    final String darkGray = "#474448";
+    final String lightGray = "#6b6d76";
+    final String white = "#f6f6f6";
 
     ///////////inserting Objects to be displayed on Survey Scene//////////////////////
 @FXML
@@ -17,6 +25,13 @@ public class MovieRentalHomePageController {
     //Opens the Account Page
     public void openAccountPage() throws Exception {
         AccountPage.display("Account Page");
+    }
+    public void handleButtonMouseEntered(ActionEvent e){
+        accountBtn.setTextFill(Color.valueOf(white));
+
+    }
+    public void handleButtonMouseExit(){
+        accountBtn.setTextFill(Color.valueOf(orange));
     }
 }//end controller
 
