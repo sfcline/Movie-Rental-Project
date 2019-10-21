35076e9f61f6df832e0213f09d625a58fa5eeaec
SLamont <stlamont3134@eagle.fgcu..edu>
2019-10-21 15:38:53 -0400

    Built Movie class as well as MovieDriver class to test also built IllegalMovieArgumentException class.

--- src/com/company/Ratings.java
+++ src/com/company/Ratings.java
@@ -1,12 +1,31 @@
 package com.company;
 
 public enum Ratings {
-    G("G"),
-    PG("PG"),
-    PG13("PG-13"),
-    R("R");
+  G("G"),
+  PG("PG"),
+  PG13("PG-13"),
+  R("R"),
+  NR("Not Rated");
 
-    public final String rating;
+  public final String rating;
 
-    Ratings(String rating) {this.rating = rating;}
+  Ratings(String rating) {
+    this.rating = rating;
+  }
+
+  public String toString() {
+    switch (this) {
+      case G:
+        return "G";
+      case PG:
+        return "PG";
+      case PG13:
+        return "PG-13";
+      case R:
+        return "R";
+      case NR:
+        return "NR";
+    }
+    return "No Rating";
+  }
 }
